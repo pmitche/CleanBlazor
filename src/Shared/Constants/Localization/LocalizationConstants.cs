491ad9b3bc456b1748df3cdb5998aa886b323346
Paul Philip Mitchell <paulpmit@gmail.com>
2023-10-18 22:18:15 +0200

    Remove all languages except English and French

--- src/Shared/Constants/Localization/LocalizationConstants.cs
+++ src/Shared/Constants/Localization/LocalizationConstants.cs
@@ -4,11 +4,7 @@ public static class LocalizationConstants
 {
     public static readonly LanguageCode[] SupportedLanguages =
     {
-        new() { Code = "en-US", DisplayName = "English" }, new() { Code = "fr-FR", DisplayName = "French" },
-        new() { Code = "km_KH", DisplayName = "Khmer" }, new() { Code = "de-DE", DisplayName = "German" },
-        new() { Code = "nl-NL", DisplayName = "Dutch - Netherlands" },
-        new() { Code = "es-ES", DisplayName = "Spanish" }, new() { Code = "ru-RU", DisplayName = "Russian" },
-        new() { Code = "sv-SE", DisplayName = "Swedish" }, new() { Code = "id-ID", DisplayName = "Indonesia" },
-        new() { Code = "it-IT", DisplayName = "Italian" }, new() { Code = "ar", DisplayName = "عربي" }
+        new() { Code = "en-US", DisplayName = "English" },
+        new() { Code = "fr-FR", DisplayName = "French" }
     };
 }
